cb499f2af4e289474a602a3f4f957b4ab804aecd
MihirPatel20 <71013825+MihirPatel20@users.noreply.github.com>
2020-10-16 02:13:10 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -5,12 +5,22 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function() {
 
     var buttonInnerHTML = this.innerHTML;
+
     makeSound(buttonInnerHTML);
+
     buttonAnimation(buttonInnerHTML);
 
   });
 
 }
+
+document.addEventListener("keypress", function(event) {
+
+  makeSound(event.key);
+
+  buttonAnimation(event.key);
+
+});
 
 
 function makeSound(key) {
@@ -50,14 +60,20 @@ function makeSound(key) {
       var kick = new Audio('sounds/kick-bass.mp3');
       kick.play();
       break;
+
+
     default: console.log(key);
 
   }
 }
 
+
 function buttonAnimation(currentKey) {
+
   var activeButton = document.querySelector("." + currentKey);
+
   activeButton.classList.add("pressed");
+
   setTimeout(function() {
     activeButton.classList.remove("pressed");
   }, 100);
